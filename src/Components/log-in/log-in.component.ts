52652a54aa3cae4571e802a1ec907ad4496126f2
marian <marian.rashed23@gmail.com>
2024-04-29 17:41:20 +0300

    the last version for login page

--- src/Components/log-in/log-in.component.ts
+++ src/Components/log-in/log-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './log-in.component.css'
 })
 export class LogInComponent {
+  constructor(private router: Router) { }
+
+  redirectToGoogle(): void {
+    window.location.href = 'https://accounts.google.com';
+  }
+  redirectToFacebook(): void {
+    window.location.href = 'https://www.facebook.com';
+  }
 
 }
