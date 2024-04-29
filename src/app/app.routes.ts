af8063066f5b07b7e0c0b6768ef8f19407631cdf
hagar <hagaribra04@gmail.com>
2024-04-29 23:59:23 +0300

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { ProfileComponent } from '../components/profile/profile.component';
+import { ChatComponent } from '../components/chat/chat.component';
+import { NotificationComponent } from '../components/notification/notification.component';
+
+export  const routes: Routes = [
+    {path:"home" , component:HomeComponent},
+    {path:"profile" , component:ProfileComponent},
+    {path:"chat" , component:ChatComponent},
+    {path:"notification" , component:NotificationComponent},
+];
