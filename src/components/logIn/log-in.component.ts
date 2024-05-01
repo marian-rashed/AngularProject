b58981bcbb61847caf2916f04d93780bf7f1c0ab
hagar <hagaribra04@gmail.com>
2024-05-01 13:19:30 +0300

    update routing

--- src/components/logIn/log-in.component.ts
+++ src/components/logIn/log-in.component.ts
@@ -1,21 +1,39 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../Services/auth.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,RouterLink],
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.css'
+  styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent {
-  constructor(private router: Router) { }
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   redirectToGoogle(): void {
     window.location.href = 'https://accounts.google.com';
   }
+
   redirectToFacebook(): void {
     window.location.href = 'https://www.facebook.com';
   }
 
+  signIn() {
+    // Call authService method to check authentication
+    const isAuthenticated = this.authService.checkIfUserIsAuthenticated();
+
+    if (isAuthenticated) {
+      this.router.navigate(['/home']); // Redirect to home page if authentication successful
+    } else {
+      // Handle authentication failure (e.g., show error message)
+      console.log('Authentication failed');
+    }
+  }
 }
