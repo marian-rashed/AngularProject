9f1b0e778d9f9799fe9b340de4e09721cd01f7b2
hagar <hagaribra04@gmail.com>
2024-05-16 15:56:15 +0300

    update sidebar

--- src/components/sidebar/sidebar.component.ts
+++ src/components/sidebar/sidebar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../Services/auth.service';
+import { ProfileService } from '../../Services/profile.service';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
@@ -9,13 +10,58 @@ import { AuthService } from '../../Services/auth.service';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
+  loggedInUserId: string  = '';
+  email: string | null = null;
+  error: string | null = null;
+  UserDataObj: any =
+    {
+      id: '',
+      email: '',
+      name: '',
+      profileImage: '',
+      coverImage: '',
+      userName: '',
+      bio: '',
+      location: ''
+    };
   showText = true;
   collapsed = false;
   iconSize = '24px'; // Initial icon size
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+    private profileService: ProfileService
+  ) {}
+  ngOnInit(): void {
+    this.fetchLoggedInUserId();
+
+
+  }
+  fetchLoggedInUserId() {
+    this.email = this.authService.getQueryParam('email');
+    console.log(this.email);
+
+    if (this.email) {
+      this.authService.getCurrentUser(this.email).subscribe({
+        next: (user) => {
+          this.loggedInUserId = user.userId;
+          this.profileService.GetUserData(this.loggedInUserId).subscribe({
+            next: (userData) => {
+              this.UserDataObj = userData.data;
+              console.log(this.UserDataObj);
+            },
+            error: (err) => {
+              console.log(err);
+            }
+          })
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }
+  }
   toggleIcons() {
     this.showText = !this.showText;
     this.iconSize = this.showText ? '24px' : '30px'; // Toggle icon size
